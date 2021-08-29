4a8c338f3ef7bd7efda1df6be9b48dc92a4f81b4
Aadarsh Verma <vaadarsh8178@gmail.com>
2021-08-29 17:03:27 +0530

    optimized images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,32 @@
 export const projects = [
   {
+    title: "Covid Tracker PWA",
+    description:
+      "React Progressive Web application built from scratch without CRA or any template serving covid data through web scrapping content using nodejs , puppetter",
+    learning:
+      "Webpack, Babel, Service Workers ,Workbox, Puppetter , PWA , Caching using SW, Mapbox",
+    image: "/images/covidTrackerPWA.webp",
+    tags: [
+      "PWA",
+      "Webpack",
+      "Babel",
+      "Service Workers",
+      "Workbox",
+      "Puppetter",
+      "Mapbox",
+      "API Caching",
+    ],
+    source: "https://ind-covid-tracker.herokuapp.com/",
+    visit: "https://github.com/Aadarsh8178/Covid_Tracker",
+    id: -1,
+  },
+  {
     title: "Angular Recipe Book",
     description:
       "You can create a recipe by going into recipe section you have to save data from manage dropdown in the navbar to save it to the firebase and later fetch using fetch data option in manage dropdown.",
     learning:
       "Angular , Learnt Angular basics , Data binding, Directives , Services , Dependency injection, Routing, Observables, Angular Forms , Pipes ,Making HTTP requests , Authentication, NgRx , Firebase ",
-    image: "/images/angular_recipe.jpeg",
+    image: "/images/angular_recipe.webp",
     tags: [
       "Angular",
       "NgRx",
@@ -23,7 +44,7 @@ export const projects = [
       "You have some initial amount using which you can buy stocks in the stocks section which will be reflected in your portfolio after ending the day in the navbar stock price will change and you can sell your stocks to earn profit , data will be saved if you click on save & load dropdown and select save and can be fetched later on clicking load data from firebase ",
     learning:
       "Vuejs , Learnt Basics of Vuejs, Data binding, Directives ,Forms , Animations and Transitions, Routing, Vuex, Vue-resource for HTTP calls",
-    image: "/images/vue_stock_trader.jpeg",
+    image: "/images/vue_stock_trader.webp",
     tags: ["Vuejs", "Vuex", "Routing in Vuejs"],
     source: "https://vue-stock-trader.vercel.app/",
     visit: "https://github.com/Aadarsh8178/vue-stock-trader",
@@ -36,7 +57,7 @@ export const projects = [
     learning: `
       Nodejs , No Npm modules , CLI Tool for admin, Self Created Template logic , Javascript , Bootstrap,
       `,
-    image: "/images/upruntime.jpeg",
+    image: "/images/upruntime.webp",
     tags: ["Nodejs", "Node Core Modules"],
     visit: "https://github.com/Aadarsh8178/UpRuntime-fileStorage-",
     id: 8,
@@ -47,7 +68,7 @@ export const projects = [
       "You can add items to favourites and then cart on selecting single item you will see option to buy which will redirect you to actual product link, data will be saved in local storage of browser, the site is incomplete and made for just learning purpose (next js) . No CSS framework is used all styles and transtions are custom css and the design is inspired by existing e-commerce sites (myntra, vegnonveg, H&M)",
     learning:
       "Nextjs , File Structure Routing in Next js, Dynamic routes , Pure CSS, React Context Api, React Hooks",
-    image: "/images/nextjs_looks.jpeg",
+    image: "/images/nextjs_looks.webp",
     tags: ["Nextjs", "CSS", "React Context Api", "React Hooks"],
     source: "https://bpc-gully-wear.vercel.app/",
     visit: "https://github.com/Aadarsh8178/GullyWear",
@@ -59,7 +80,7 @@ export const projects = [
       "You can customise your burger from controls given and order it after signing in order will be reflected in orders page",
     learning:
       "React , Class based React components, LifeCycle of React Components, Higher Order function , Error handling, React-Router, Redux , Redux Thunk , Firebase Realtime database, Firebase Authentication",
-    image: "/images/react_burger_builder.jpeg",
+    image: "/images/react_burger_builder.webp",
     tags: [
       "React",
       "Redux",
@@ -78,7 +99,7 @@ export const projects = [
       "You can input your location and it will give you weather report for it, this was the very first project that I build during starting of my programming journey",
     learning:
       "Nodejs, Event loop , Express, HBS templates, Handling Errors, Callback Chaining, Promises, Application deployment , Environment variables",
-    image: "/images/weather_app.jpeg",
+    image: "/images/weather_app.webp",
     tags: ["Nodejs", "Express"],
     source: "https://bpc-weather-application.herokuapp.com/",
     visit: "https://github.com/Aadarsh8178/nodejs-weather-webserver",
@@ -117,7 +138,7 @@ export const projects = [
     learning: `
       Nodejs , WebSockets, Socket.io
       `,
-    image: "/images/chat_app.jpeg",
+    image: "/images/chat_app.webp",
     tags: ["Nodejs", "WebSockets", "Socket.io"],
     source: "https://bpc-chat-app.herokuapp.com/",
     visit: "https://github.com/Aadarsh8178/Chat-app",
@@ -130,7 +151,7 @@ export const projects = [
     learning: `
      Javascript , Bootstrap 
       `,
-    image: "/images/github_finder.jpeg",
+    image: "/images/github_finder.webp",
     tags: ["Javascript", "Bootstrap"],
     source: "https://bpc-github-finder.herokuapp.com/",
     visit: "https://github.com/Aadarsh8178/Github-Finder",
@@ -173,14 +194,14 @@ export const ExperienceTimeLineData = [
     duration: "Jan - May",
     text: "SDE 1 Intern",
     company: "Exxat Systems",
-    tech: ["Angular", "C#", ".NET", "Azure Cosmos"],
+    tech: ["Angular", "C#", ".NET"],
   },
   {
     year: "2021",
     duration: "June - *",
     text: "SDE 1",
     company: "Exxat Systems",
-    tech: ["Angular", "C#", ".NET", "Azure Cosmos"],
+    tech: ["Angular", "C#", ".NET"],
   },
 ];
 
@@ -203,7 +224,7 @@ export const EducationTimeLineData = [
 export const companies = [
   {
     desc: [
-      "Solved more than 70 Work Items including bugs, user stories and tasks",
+      "Solved more than 200 Work Items including bugs, user stories and tasks",
       "Optimized app by removing template function calls and duplicate code by introducing services",
       "Developed Api on .Net Framework",
     ],
@@ -285,12 +306,12 @@ export const articles = [
   {
     title:
       "Uploading and Downloading Multiple Files and Handling CORS error on Firebase",
-    img: "/images/react-firebase.png",
+    img: "/images/react-firebase.webp",
     link: "https://vaadarsh8178.medium.com/uploading-and-downloading-multiple-files-and-handling-cors-error-on-firebase-405779abe543",
   },
   {
     title: "Handling Custom SVGs in React using Styled Components",
-    img: "/images/stylecomponents.png",
+    img: "/images/stylecomponents.webp",
     link: "https://vaadarsh8178.medium.com/handling-custom-svgs-in-react-using-styled-components-30d2739ff4cb",
   },
 ];
